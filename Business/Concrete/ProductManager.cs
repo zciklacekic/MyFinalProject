ec79ab83003d3adadb8f1d13350f695769fa675a
zciklacekic <zciklacekic@gmail.com>
2021-02-01 15:34:40 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Is Kodları
+            //Yetki ve is kontrolleri yapıldıktan sonra aşağıdaki kodlar çalışacak
+
+            return _productDal.GetAll();
+        }
+    }
+}

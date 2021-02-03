99f0d0a16cdfaebc59f586894db353b72f43a198
zciklacekic <zciklacekic@gmail.com>
2021-02-03 23:47:04 +0300

    02.03.2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,19 +1,26 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle 
+    //Yazılıma yeni bir ozellik eklediğinde mevcuttaki kodları değiştirme
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll())
+            //ProductManager productManager = new ProductManager(new InMemoryProductDal()); 
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            //foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetAllByCategoryId(2))
+            //foreach (var product in productManager.GetByUnitPrice(40,100))
             {
                 Console.WriteLine(product.ProductName);
             }
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Genetic Contraint
+    //class: Referans tip olabilir where T:class
+    //IEntity olabilir veya IEntity i implemet eden bir class olabilir where T:class:IEntity
+    //new() new lenebilir olmalı --Yani Interface in kendisi değil onu implement eden bir class olabilir.--  where T:class,IEntity,new()
+    //Boylece T nin newlenemez ve IEntity i implement eden bir class olmasını saglayarak kısıtladık
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
